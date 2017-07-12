cead26a9ab289ec45f4fdd9f69c03cffc6befe66
ArBond <bonartembon@gmail.com>
2017-07-12 21:42:20 +0300

    hw6_27.06.17

---
+++ Bondarenko Artemi/hw6_27.06.17/calculator.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "operation.h"
+#include <vector>
+
+
+class Calculator
+{
+private:
+	std::vector<Operation*> m_operations = { 
+		new SummOperation(), new DeductOperation(),
+		new DivideOperation(), new MultiplyOperation(),
+		new SinusOperation(), new CosineOperation(),
+		new ExponentOperation() };
+
+public:
+	void ShowMenu()
+	{
+		int count = 1;
+		for (auto it : m_operations)
+		{
+			std::cout << count++ << " - " << it->GetName() << "\n";
+		}
+
+		std::cout << "0 - Exit\nPlease choose operation: ";
+	}
+
+	std::vector<Operation*>::iterator Begin()
+	{
+		auto it = m_operations.begin();
+		return it;
+	}
+
+	int GetOperationCuantity()
+	{
+		return m_operations.size();
+	}
+
+	void ExecuteOperation(Operation* operation)
+	{
+		operation->Execute();
+	}
+};
